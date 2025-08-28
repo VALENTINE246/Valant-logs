65c1c24a316e58f67514881208c19a1dca9091ba
VALENTINE246 <somtoifeduba2@gmail.com>
2025-08-28 16:45:08 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,22 @@
+const contactUs = document.getElementById('contact-us')
+
+contactUs.addEventListener('click', () => {
+  // Redirect to the link
+  window.location.href = 'https://wa.me/+2349031444628';
+});
+
+
+const knowMore = document.getElementById('know-more')
+
+
+knowmoreInfo.className = 'know-more'
+
+knowMore.addEventListener('click', () => {
+
+  const knowmoreInfo = document.createElement('div')
+
+  knowmoreInfo = 'Are you a working guy, Do you need a sharp FB accounts for your hustle, We got you covered, We deliver variety of FB accounts, Instagram, TikTok, Foreign numbers, VPN, Account boosting and other tools';
+  
+  knowmoreInfo.style.display = 'flex'
+
+})
