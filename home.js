b03082a5025ba4a25c8fda16a1650a2adee70aba
VALENTINE246 <somtoifeduba2@gmail.com>
2025-08-28 16:52:59 +0100

    Update home.js

--- home.js
+++ home.js
@@ -1,10 +1,14 @@
 const contactUs = document.getElementById('contact-us')
 
+
 contactUs.addEventListener('click', () => {
   // Redirect to the link
-  window.location.href = 'https://wa.me/+2349031444628';
+  window.location.href = whatsappLink;
 });
 
+const phoneNumber = '+1234567890';
+const captionText = 'Hello! Can i get more info your';
+const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(captionText)}`;
 
 const knowMore = document.getElementById('know-more')
 
@@ -18,5 +22,6 @@ knowMore.addEventListener('click', () => {
   knowmoreInfo = 'Are you a working guy, Do you need a sharp FB accounts for your hustle, We got you covered, We deliver variety of FB accounts, Instagram, TikTok, Foreign numbers, VPN, Account boosting and other tools';
   
   knowmoreInfo.style.display = 'flex'
+
 
 })
